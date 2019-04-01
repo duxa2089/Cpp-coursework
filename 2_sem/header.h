61009db59ee6b98f1a2f7c4bac42c561c1f66c4f
duxa2089 <31611618+duxa2089@users.noreply.github.com>
2019-04-01 17:08:25 +0300

    Updated design of code

--- 2_sem/header.h
+++ 2_sem/header.h
@@ -1,4 +1,4 @@
-#ifndef headerh // заголовочный файл. Подключение необходимых библиотек, создание классов и функций
+#ifndef headerh // Header file. Connection of necessary libraries, creation of classes and functions
 #define headerh
 #include <iostream>
 #include <Windows.h>
@@ -6,26 +6,26 @@
 #include <fstream>
 #include <string>
 using namespace std;
-class  Product //класс товаров
+class  Product // Сlass of goods 
 {
  public:
-    char name[100]; //имя товара
-    char article[100]; //артикул товара
-    int count; //количество товара
-    double price; //цена товара
-    ~Product() {};//деструктов товара
-    Product(); // конструктор ввода информации о товаре
-    Product(int empty); // конструктор для обмена
-    Product(int empty1, int empty2); //конструктор для указания артикула
-    printInfo(); //функция вывода информации о товаре
-    friend istream& operator>>(istream &s,Product &v); //перегрузка операции ввода
+    char name[100]; // Name of goods
+    char article[100]; // Article of goods
+    int count; // Amount of goods
+    double price; // Price of goods
+    ~Product() {};// The destructor of the product
+    Product(); // Designer input information about the product
+    Product(int empty); // The constructor for the exchange
+    Product(int empty1, int empty2); // Constructor to specify the article
+    printInfo(); // A function display information about the product
+    friend istream& operator >> (istream &s,Product &v); // Input operation overload
 };
-void FileInput(vector <Product*> &productvector); //проверка файла на уже имеющиеся в нём товары
-void FileOutput(vector <Product*> &productvector); //Запись товаров в файл
-void Main(vector <Product*> &productvector); //Меню
-void Case1(vector <Product*> &productvector); //первая функция кейса
-int Case2(vector <Product*> &productvector);  //вторая функция в кейсе
-int Case3(vector <Product*> &productvector);  //третья функция в кейсе
-void Case4(vector <Product*> &productvector); //четвёртая функция в кейсе
-void Case5(vector <Product*> &productvector); //пятая функция в кейсе
+void FileInput(vector <Product*> &productvector); // Checking a file for products already in it
+void FileOutput(vector <Product*> &productvector); // The entry of goods into a file
+void Main(vector <Product*> &productvector); // Menu
+void Case1(vector <Product*> &productvector);
+int Case2(vector <Product*> &productvector);
+int Case3(vector <Product*> &productvector); 
+void Case4(vector <Product*> &productvector);
+void Case5(vector <Product*> &productvector);
 #endif
